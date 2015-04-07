08ba3e042f5176702575d349b6a05121274cc31f
EC2 Default User <ec2-user@ip-172-31-4-98.us-west-2.compute.internal>
2015-04-07 04:58:58 +0000

    new file

---
+++ connect.php
@@ -0,0 +1,26 @@
+<?php 
+define('DB_HOST', 'localhost:3306'); 
+define('DB_NAME', 'portfolio'); 
+define('DB_USER','root'); 
+define('DB_PASSWORD','invest1bil'); 
+$con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error()); 
+$db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error()); 
+
+//inserting Record to the database 
+//when the user enter data to the contact.html form and press Send Message button, so the data will be come to this connectivity.php file and store in $name, $email, and $message variables.
+
+$name = $_POST['name']; 
+$email = $_POST['email']; 
+$message = $_POST['message']; 
+$query = "INSERT INTO loginInfo(name,email) VALUES('$name','$email')"; 
+$result = mysql_query($query); 
+
+if($result) 
+	{
+ 	echo "Successfully updated database";
+	echo "Thank you for contacting. For fast response, please contact me directly at chuankengchou@gmail.com"; 
+ 	} 
+	else { die('Error: '.mysql_error($con)); 
+	} 
+	mysql_close($con); 
+?>
