706540657099b8efa887a81fe35c7d1aca9de07e
Jimmy Chou <chuankengchou@gmail.com>
2017-05-07 22:29:02 -0400

    remove old files

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 define('DB_HOST', 'localhost:3306'); 
 define('DB_NAME', 'portfolio'); 
 define('DB_USER','root'); 
-define('DB_PASSWORD','invest1bil'); 
+define('DB_PASSWORD',''); 
 $con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error()); 
 $db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error()); 
 
